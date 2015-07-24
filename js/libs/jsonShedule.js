5efe5b498127feaac5ab4081ca4410caedbdc9bc
Dmitry Bondarchuk <dvbondarchuk@gmail.com>
2015-07-24 18:58:31 +0500

    Styles added. Init updated

--- js/libs/jsonShedule.js
+++ js/libs/jsonShedule.js
@@ -1,12 +1,34 @@
+Number.prototype.zeroPad = function(length) {
+   length = length || 2; // defaults to 2 if no parameter is passed
+   return (new Array(length).join('0')+this).slice(length*-1);
+};
 (function( $ ) {
+	var days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
 	var methods = {
 		init: function( options ) {
-
+			var $table = $('<table />');
+			for(j = 7; j < 24; j++) {
+				$tr = $('<tr />')
+				if(j == 7) {
+					$tr.append($('<td />'));
+				} else {
+					$tr.append("<td>" + j.zeroPad() + ":00</td>");
+				}
+				for(var i = 0; i < days.length; i++) {
+					if(j != 7) {
+						$tr.append($('<td />'));
+					} else {
+						$tr.append("<td>" + days[i] + "</td>");
+					}
+				}
+				$table.append($tr);
+			} 
+			this.append($table);
 		}, 
 		add: function( options ) {
 			
 		}
-	}
+	};
 	$.fn.jsonShedule = function( method ) {
 		if ( methods[method] ) {
 			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
